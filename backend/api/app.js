0313d21aad610f3a05ee80e43b544b5e955dabf6
NOOB-3301 <sepic466@gmail.com>
2025-02-23 03:42:47 +0530

    fix: update CORS configuration to allow all origins and enable credentials

--- backend/api/app.js
+++ backend/api/app.js
@@ -3,7 +3,7 @@ import cors from 'cors'
 
 const app = express()
 
-app.use(cors())
+app.use(cors({ origin: '*', credentials: true }));
 app.use(express.json({ limit: '16kb' }))
 app.use(express.urlencoded({ extended: true }))
 
